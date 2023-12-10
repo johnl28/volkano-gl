d6eff57b630fd4a17b71bb4e9557b9f4c2f2194d
John Criste <54412955+johnl28@users.noreply.github.com>
2023-12-10 19:57:47 +0000

    Create default shaders

---
+++ volkano-gl/OpenGL-Core/shaders/default_frag.glsl
@@ -0,0 +1,10 @@
+#version 400 core
+
+out vec4 FragColor;
+
+void main()
+{
+	FragColor = vec4(1.0f, 1.0f, 1.0f, 1.0f);
+}
+
+
---
+++ volkano-gl/OpenGL-Core/shaders/default_vert.glsl
@@ -0,0 +1,8 @@
+#version 400 core
+layout (location = 0) in vec3 inPos;
+
+void main()
+{
+	gl_Position = vec4(inPos.x, inPos.y, inPos.z, 1.0f);
+
+}
