abe4b32eca7bb1b57e88176cc03faaf9b15018bb
Johnl <ionutcriste28@gmail.com>
2023-12-15 17:12:43 +0000

    Index Bufferr Abstraction

---
+++ volkano-gl/OpenGL-Core/src/IndexBuffer.h
@@ -0,0 +1,19 @@
+#pragma once
+#include "glad/glad.h"
+
+class IndexBuffer
+{
+public:
+	IndexBuffer(const GLuint* data, GLuint count);
+	~IndexBuffer();
+
+	void Bind() const;
+	void UnBind() const;
+
+	GLuint GetCount() const { return m_count; }
+
+private:
+	GLuint m_bufferID = 0;
+	GLuint m_count = 0;
+};
+
