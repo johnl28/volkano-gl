2f3ebc7eacf94ec410673b3aa04b42c04f93285d
Johnl <ionutcriste28@gmail.com>
2023-12-15 17:29:08 +0000

    Update default_vert.glsl

--- volkano-gl/VolkanoSandbox/assets/shaders/default_vert.glsl
+++ volkano-gl/VolkanoSandbox/assets/shaders/default_vert.glsl
@@ -5,8 +5,10 @@ layout (location = 1) in vec2 verTextureCoord;
 
 out vec2 FragTextureCoord;
 
+uniform mat4 u_Transform;
+
 void main()
 {
-	gl_Position = vec4(verPos.x, verPos.y, verPos.z, 1.0f);
+	gl_Position = u_Transform * vec4(verPos, 1.0f);
 	FragTextureCoord = verTextureCoord;
 }
