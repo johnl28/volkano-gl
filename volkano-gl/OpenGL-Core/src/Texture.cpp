41afd8bd51ac7d535b6a1d79f9672d394f1ab12b
John Criste <54412955+johnl28@users.noreply.github.com>
2023-12-17 00:01:44 +0000

    Update Texture.cpp

--- volkano-gl/OpenGL-Core/src/Texture.cpp
+++ volkano-gl/OpenGL-Core/src/Texture.cpp
@@ -3,6 +3,7 @@
 
 
 namespace glcore {
+
 	Texture::Texture(const std::string& texturePath, bool flip)
 	{
 		if (flip)
@@ -22,8 +23,8 @@ namespace glcore {
 
 		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
 		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
-		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
 
 		glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA8, m_width, m_height, 0, GL_RGBA, GL_UNSIGNED_BYTE, textureBuffer);
 		stbi_image_free(textureBuffer);
