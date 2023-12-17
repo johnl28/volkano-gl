3a9c8defa1529f2365ca1d84cca8ea3107e214dc
John Criste <54412955+johnl28@users.noreply.github.com>
2023-12-17 00:01:27 +0000

    Add View and Projection matrix

--- volkano-gl/OpenGL-Core/src/GLApplication.cpp
+++ volkano-gl/OpenGL-Core/src/GLApplication.cpp
@@ -1,5 +1,6 @@
 
 #include "glm/gtc/matrix_transform.hpp"
+#include "glm/glm.hpp"
 
 #include "GLApplication.h"
 #include "Log.h"
@@ -12,12 +13,88 @@
 #include "Shader.h"
 #include "ShaderProgram.h"
 
+
 namespace glcore {
 
+	glm::mat4 view = glm::mat4(1.0f);
+	glm::mat4 trans = glm::mat4(1.0f);
+	ShaderProgram* sp;
 
 	void GlErrorCallback(int error_code, const char* description)
 	{
 		GLCORE_ERR("[OpenGL]: Error code %d. %s", error_code, description);
+	}
+
+	void KeyCallback(GLFWwindow* window, int key, int scancode, int action, int mods)
+	{
+		switch (key)
+		{
+		case GLFW_KEY_W:
+			trans = glm::rotate(trans, 10.0f, glm::vec3(1.0f, 0.0f, 0.0f));
+			break;
+		case GLFW_KEY_S:
+			trans = glm::rotate(trans, -10.0f, glm::vec3(1.0f, 0.0f, 0.0f));
+			break;
+		case GLFW_KEY_A:
+			trans = glm::rotate(trans, 10.0f, glm::vec3(0.0f, 1.0f, 0.0f));
+			break;
+		case GLFW_KEY_D:
+			trans = glm::rotate(trans, -10.0f, glm::vec3(0.0f, 1.0f, 0.0f));
+			break;
+		default:
+			return;
+		}
+
+		//sp->SetUniformMatrix4fv("u_View", view);
+		sp->SetUniformMatrix4fv("u_Transform", trans);
+	}
+
+	void MouseMove(GLFWwindow* window, double xpos, double ypos)
+	{
+		if (!sp) {
+			return;
+		}
+		
+		static double lastX = 0.0;
+		static double lastY = 0.0;
+
+		static GLfloat cameraVelocity = 0.02f;
+
+
+		if (glfwGetMouseButton(window, GLFW_MOUSE_BUTTON_LEFT) == GLFW_PRESS)
+		{
+			GLfloat x = 0.0f;
+			GLfloat y = 0.0f;
+			GLfloat z = 0.0f;
+
+			if(glfwGetKey(window, GLFW_KEY_X) == GLFW_PRESS)
+			{
+				x = lastX < xpos ? cameraVelocity : -cameraVelocity;
+			}
+			else if(glfwGetKey(window, GLFW_KEY_Y) == GLFW_PRESS)
+			{
+				y = lastY < ypos ? -cameraVelocity : cameraVelocity;
+			}
+			else if (glfwGetKey(window, GLFW_KEY_Z) == GLFW_PRESS)
+			{
+				z = lastY < ypos ? -cameraVelocity : cameraVelocity;
+				z *= 5.0f;
+			}
+				
+
+			view = glm::translate(view, glm::vec3(x, y, z));
+		}
+
+		lastX = xpos;
+		lastY = ypos;
+
+		sp->SetUniformMatrix4fv("u_View", view);
+	}
+
+	void MouseScroll(GLFWwindow* window, double xoffset, double yoffset)
+	{
+		GLCORE_INFO("SCROLL");
+		GLCORE_ERR("AAAAAAAAAAAAAAAAAAAA");
 	}
 
 
@@ -36,81 +113,6 @@ namespace glcore {
 		glfwTerminate();
 	}
 
-	void GLApplication::Run()
-	{
-		if (!m_initialised)
-		{
-			GLCORE_ERR("[GLApplication] Application loop cannot run if initialisation failed.");
-			return;
-		}
-
-		Vertex vertices[] = {
-
-			{
-				glm::vec3 { -0.5f, -0.5f, 1.0f },
-				glm::vec2 { 0.0f, 1.0f },
-			},
-
-			{
-				glm::vec3 { 0.5f, -0.5f, 1.0f },
-				glm::vec2 { 1.0f, 1.0f },
-			},
-
-			{
-				glm::vec3 { -0.5f, 0.5f, 1.0f },
-				glm::vec2 { 0.0f, 0.0f },
-			},
-
-			{
-				glm::vec3 { 0.5f, 0.5f, 1.0f },
-				glm::vec2 { 1.0f, 0.0f }
-			}
-		};
-
-
-
-		GLuint indicies[] = {
-			0, 1, 2,
-			1, 3, 2
-		};
-
-		VertexArray va;
-		VertexBuffer vb(vertices, sizeof(vertices));
-		IndexBuffer ib(indicies, sizeof(indicies) / sizeof(GLuint));
-
-		va.AddAttribute<GLfloat>(3);
-		va.AddAttribute<GLfloat>(2);
-		va.AddAttribute<GLfloat>(3);
-		va.CreateAttribPointers();
-
-		ShaderProgram program;
-		program.LoadShaders("assets/shaders/default_vert.glsl", "assets/shaders/default_frag.glsl");
-		program.Bind();
-
-		Texture texture("assets/textures/brick_wall2.jpg");
-		texture.Bind(0);
-
-		glm::mat4 trans = glm::mat4(1.0f);
-		trans = glm::translate(trans, glm::vec3(0.3, 0.2, 0.0f));
-		trans = glm::rotate(trans, 90.0f, glm::vec3(0.0, 0.0, 1.0));
-		program.SetUniformMatrix4fv("u_Transform", trans);
-
-		
-		while (!glfwWindowShouldClose(m_window))
-		{
-			glClear(GL_COLOR_BUFFER_BIT);
-
-
-			glDrawElements(GL_TRIANGLES, ib.GetCount(), GL_UNSIGNED_INT, nullptr);
-
-			glfwSwapBuffers(m_window);
-
-			glfwPollEvents();
-		}
-
-	}
-
-
 	void GLApplication::InitGLFW()
 	{
 		if (glfwInit() != GLFW_TRUE)
@@ -127,7 +129,7 @@ namespace glcore {
 
 
 		m_window = glfwCreateWindow(m_width, m_height, m_title.c_str(), NULL, NULL);
-		if(m_window == nullptr)
+		if (m_window == nullptr)
 		{
 			GLCORE_ERR("[GLApplication] Failed to create window");
 			glfwTerminate();
@@ -142,7 +144,14 @@ namespace glcore {
 			return;
 		}
 
+		glEnable(GL_DEPTH_TEST);
+
+		//glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
 		glViewport(0, 0, m_width, m_height);
+
+		glfwSetScrollCallback(m_window, MouseScroll);
+		glfwSetCursorPosCallback(m_window, MouseMove);
+		glfwSetKeyCallback(m_window, KeyCallback);
 
 		m_initialised = true;
 
@@ -152,6 +161,133 @@ namespace glcore {
 		GLCORE_INFO((char*)version);
 
 	}
+
+	void GLApplication::Run()
+	{
+		if (!m_initialised)
+		{
+			GLCORE_ERR("[GLApplication] Application loop cannot run if initialisation failed.");
+			return;
+		}
+
+		Vertex vertices[] = {
+
+			// front face
+			{
+				glm::vec3 { -1.0f, -1.0f, 1.0f },
+				glm::vec2 { 0.0f, 1.0f },
+			},
+
+			{
+				glm::vec3 { 1.0f, -1.0f, 1.0f },
+				glm::vec2 { 1.0f, 1.0f },
+			},
+
+			{
+				glm::vec3 { -1.0f, 1.0f, 1.0f },
+				glm::vec2 { 0.0f, 0.0f },
+			},
+
+			{
+				glm::vec3 { 1.0f, 1.0f, 1.0f },
+				glm::vec2 { 1.0f, 0.0f }
+			},
+
+
+			// back face
+			{
+				glm::vec3 { -1.0f, -1.0f, -1.0f },
+				glm::vec2 { 1.0f, 1.0f },
+			},
+
+			{
+				glm::vec3 { 1.0f, -1.0f, -1.0f },
+				glm::vec2 { 0.0f, 1.0f },
+
+			},
+
+			{
+				glm::vec3 { -1.0f, 1.0f, -1.0f },
+				glm::vec2 { 1.0f, 0.0f }
+			},
+
+			{
+				glm::vec3 { 1.0f, 1.0f, -1.0f },
+			
+				glm::vec2 { 0.0f, 0.0f },
+			},
+		};
+
+
+
+		GLuint indicies[] = {
+			// front
+			0, 1, 2,
+			1, 3, 2,
+
+			// back
+			4, 5, 6,
+			5, 7, 6,
+
+			// left
+			4, 0, 6,
+			0, 2, 6,
+
+			// right
+			5, 1, 7,
+			7, 3, 1,
+
+			// top
+			2, 3, 6,
+			
+		};
+
+		VertexArray va;
+		VertexBuffer vb(vertices, sizeof(vertices));
+		IndexBuffer ib(indicies, sizeof(indicies) / sizeof(GLuint));
+
+
+
+		va.AddAttribute<GLfloat>(3);
+		va.AddAttribute<GLfloat>(2);
+		va.AddAttribute<GLfloat>(3);
+		va.CreateAttribPointers();
+
+		sp = new ShaderProgram();
+		sp->LoadShaders("assets/shaders/default_vert.glsl", "assets/shaders/default_frag.glsl");
+		sp->Bind();
+
+		Texture texture("assets/textures/uv_grid_opengl.jpg");
+		texture.Bind(0);
+
+		glm::mat4 trans = glm::mat4(1.0f);
+		trans = glm::rotate(trans, 30.0f, glm::vec3(0.0f, 1.0f, 0.0f));
+		sp->SetUniformMatrix4fv("u_Transform", trans);
+
+		glm::mat4 proj = glm::perspective(glm::radians(95.0f), (float)m_width /
+			(float)m_height, 0.1f, 1000.0f);
+		
+		sp->SetUniformMatrix4fv("u_Projection", proj);
+
+		view = glm::translate(view, glm::vec3(0.0f, 0.0f, -50.1f));
+		//view = glm::rotate(view, 30.0f, glm::vec3(1.0f, 0.0f, 0.0f));
+		sp->SetUniformMatrix4fv("u_View", view);
+
+
+		while (!glfwWindowShouldClose(m_window))
+		{
+			glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+
+			glDrawElements(GL_TRIANGLES, ib.GetCount(), GL_UNSIGNED_INT, nullptr);
+
+			glfwSwapBuffers(m_window);
+
+			glfwPollEvents();
+		}
+
+	}
+
 
 	void GLApplication::SetSrollCallback(GLFWscrollfun func)
 	{
--- volkano-gl/VolkanoSandbox/assets/shaders/default_vert.glsl
+++ volkano-gl/VolkanoSandbox/assets/shaders/default_vert.glsl
@@ -7,11 +7,13 @@ layout (location = 3) in vec3 verNormals;
 out vec2 FragTextureCoord;
 out vec3 FragNormals;
 
+uniform mat4 u_Projection;
+uniform mat4 u_View;
 uniform mat4 u_Transform;
 
 void main()
 {
-	gl_Position = u_Transform * vec4(verPos, 1.0f);
+	gl_Position = u_Projection * u_View * u_Transform * vec4(verPos, 1.0f);
 
 	FragTextureCoord = verTextureCoord;
 	FragNormals = verNormals;
