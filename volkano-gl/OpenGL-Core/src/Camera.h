ac02ee3d10a22b1007dac58a3c258062928b2a71
John Criste <54412955+johnl28@users.noreply.github.com>
2023-12-26 23:05:24 +0000

    Update Camera Speed

--- volkano-gl/OpenGL-Core/src/Camera.h
+++ volkano-gl/OpenGL-Core/src/Camera.h
@@ -52,7 +52,7 @@ namespace glcore {
 	private:
 		float m_Yaw = -90.0f;
 		float m_Pitch = 0.0f;
-		float m_CameraSpeed = 0.5f;
+		float m_CameraSpeed = 0.1f;
 
 		glm::vec3 m_CameraUp;
 		glm::vec3 m_CameraFront;
