f69189f4f1c65dabf4ff08c381f91786d6885f40
Johnl <ionutcriste28@gmail.com>
2023-12-26 15:32:41 +0000

    Ambient, Diffuse and Specular Light

--- volkano-gl/VolkanoSandbox/assets/shaders/default_frag.glsl
+++ volkano-gl/VolkanoSandbox/assets/shaders/default_frag.glsl
@@ -2,14 +2,47 @@
 
 in vec2 FragTextureCoord;
 in vec3 FragNormals;
+in vec3 FragPos;
 
 out vec4 FragColor;
 
 uniform sampler2D u_Texture;
 
+uniform vec3 u_LightPositon;       
+uniform vec3 u_LightColor;
+uniform vec3 u_ViewPos;
+
 void main()
 {
-	FragColor = texture(u_Texture, FragTextureCoord) + vec4(0.0f, 0.0f, 0.0f, 1.0f);
+    vec3 u_LightColor = vec3(1.0);
+	vec3 textureColor = texture(u_Texture, FragTextureCoord).rgb;
+
+    // Ambient
+    float ambientStrength = 0.1;
+    vec3 ambient = ambientStrength * u_LightColor;
+
+    // Diffuse
+    vec3 lightDir = normalize(u_LightPositon - FragPos);
+
+	vec3 norm = normalize(FragNormals);
+    float diff = max(dot(norm, lightDir), 0.0);
+    
+    vec3 diffuse = diff * u_LightColor;
+    
+
+    // Specular
+    float specularStrength = 0.5;
+    vec3 viewDir = normalize(u_ViewPos - FragPos);
+    vec3 reflectDir = reflect(-lightDir, norm);
+
+    float spec = pow(max(dot(viewDir, reflectDir), 0.0), 32);
+    vec3 specular = specularStrength * spec * u_LightColor;
+
+
+    vec3 result = (ambient + diffuse + specular) * textureColor;
+
+
+	FragColor = vec4(result, 1.0);
 }
 
 
--- volkano-gl/VolkanoSandbox/assets/shaders/default_vert.glsl
+++ volkano-gl/VolkanoSandbox/assets/shaders/default_vert.glsl
@@ -1,11 +1,12 @@
 #version 460 core
 
-layout (location = 0) in vec3 verPos;
-layout (location = 1) in vec2 verTextureCoord;
-layout (location = 3) in vec3 verNormals;
+layout (location = 0) in vec3 aPos;
+layout (location = 1) in vec2 aTextureCoord;
+layout (location = 2) in vec3 aNormals;
 
 out vec2 FragTextureCoord;
 out vec3 FragNormals;
+out vec3 FragPos;
 
 uniform mat4 u_Projection;
 uniform mat4 u_View;
@@ -13,9 +14,11 @@ uniform mat4 u_Transform;
 
 void main()
 {
-	gl_Position = u_Projection * u_View * u_Transform * vec4(verPos, 1.0);
+	gl_Position = u_Projection * u_View * u_Transform * vec4(aPos, 1.0);
 
-	FragTextureCoord = verTextureCoord;
-	FragNormals = verNormals;
+	FragTextureCoord = aTextureCoord;
+
+	FragPos = vec3(u_Transform * vec4(aPos, 1.0));
+	FragNormals = mat3(transpose(inverse(u_Transform))) * aNormals;;
 }
 
