ad1f467fe67dbb0b69912aff749c5d8a030889af
John Criste <54412955+johnl28@users.noreply.github.com>
2023-12-28 02:16:47 +0000

    Removing redundant lamp shader code

--- volkano-gl/VolkanoSandbox/assets/shaders/lamp_frag.glsl
+++ volkano-gl/VolkanoSandbox/assets/shaders/lamp_frag.glsl
@@ -1,16 +1,11 @@
 #version 460 core
 
-in vec2 FragTextureCoord;
-
 out vec4 FragColor;
-
-uniform sampler2D u_Texture;
 
 uniform vec3 u_LightColor;
 
 void main()
 {
-    vec3 u_LightColor = vec3(1.0);
 	FragColor = vec4(u_LightColor, 1.0f);
 }
 
--- volkano-gl/VolkanoSandbox/assets/shaders/lamp_vert.glsl
+++ volkano-gl/VolkanoSandbox/assets/shaders/lamp_vert.glsl
@@ -1,10 +1,6 @@
 #version 460 core
 
 layout (location = 0) in vec3 aPos;
-layout (location = 1) in vec2 aTextureCoord;
-layout (location = 2) in vec3 aNormals;
-
-out vec2 FragTextureCoord;
 
 uniform mat4 u_Projection;
 uniform mat4 u_View;
@@ -13,8 +9,5 @@ uniform mat4 u_Transform;
 void main()
 {
 	gl_Position = u_Projection * u_View * u_Transform * vec4(aPos, 1.0);
-
-	FragTextureCoord = aTextureCoord;
-
 }
 
