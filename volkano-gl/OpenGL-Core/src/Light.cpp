20867122327774a6370cab1c833bcfe65285e89e
John Criste <54412955+johnl28@users.noreply.github.com>
2023-12-28 02:15:30 +0000

    Light class

--- volkano-gl/OpenGL-Core/src/Light.cpp
+++ volkano-gl/OpenGL-Core/src/Light.cpp
@@ -3,6 +3,15 @@
 
 namespace glcore {
 
+	Light::Light()
+	{
+		m_Model = std::make_unique<Model>();
+		m_Model->Load("assets/models/shapes/sphere.fbx");
+	}
 
+	void Light::Render(ShaderProgram* shader)
+	{
+		m_Model->Render(shader);
+	}
 
 }
--- volkano-gl/OpenGL-Core/src/Light.h
+++ volkano-gl/OpenGL-Core/src/Light.h
@@ -1,10 +1,22 @@
 #pragma once
+#include "glm/glm.hpp"
 
+#include "Core/ShaderProgram.h"
+#include "Model.h"
 
 namespace glcore {
 
 	class Light
 	{
+	public:
+		Light();
+		void Render(ShaderProgram *shader);
+
+		const glm::vec3& GetColor() const { return m_Color; }
+
+	private:
+		std::unique_ptr<Model> m_Model = nullptr;
+		glm::vec3 m_Color = glm::vec3(1.0f, 1.0f, 1.0f);
 	};
 
 }
