6031a44d70b399293e931796ece56021218eb60d
John Criste <54412955+johnl28@users.noreply.github.com>
2023-12-28 23:00:48 +0000

    Update Log.cpp

--- volkano-gl/OpenGL-Core/src/Log.cpp
+++ volkano-gl/OpenGL-Core/src/Log.cpp
@@ -18,18 +18,26 @@ namespace glcore
 
         switch (type) 
         {
-        case LogType::INFO:
-            std::cout << "\033[37m[INFO] ";
-            break;
-        case LogType::DEBUG:
-            std::cout << "\033[34m[DEBUG] ";
-            break;
-        case LogType::WARNING:
-            std::cout << "\033[33m[WARNING] ";
-            break;
-        case LogType::ERROR:
-            std::cout << "\033[31m[ERROR] ";
-            break;
+            case LogType::INFO:
+            {
+                std::cout << "\033[37m[INFO] ";
+                break;
+            }
+            case LogType::DEBUG:
+            {
+                std::cout << "\033[34m[DEBUG] ";
+                break;
+            }
+            case LogType::WARNING:
+            {
+                std::cout << "\033[33m[WARNING] ";
+                break;
+            }
+            case LogType::ERROR:
+            {
+                std::cout << "\033[31m[ERROR] ";
+                break;
+            }
         }
 
         std::cout << buffer << "\033[0m" << std::endl;
