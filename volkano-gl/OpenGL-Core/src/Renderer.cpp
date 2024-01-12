d82442d1d892f8c7e09a8e154543b8cf4ea15da6
Johnl <ionutcriste28@gmail.com>
2024-01-12 20:39:19 +0000

    Renderer Implementation

--- volkano-gl/OpenGL-Core/src/Renderer.cpp
+++ volkano-gl/OpenGL-Core/src/Renderer.cpp
@@ -1,9 +1,29 @@
 #include "Renderer.h"
-
-
+#include "Mesh.h"
+#include "Log.h"
 
 namespace glcore {
 
+	void Renderer::RenderModel(Model* model, Camera* camera, ShaderProgram* shader)
+	{
+		if (!model || !camera || !shader)
+		{
+			GLCORE_WARN("[Renderer] Cannot render model, Model: %d, Camera %d, Shader %d", model, camera, shader);
+			return;
+		}
+
+		shader->Bind();
+		shader->SetUniformMatrix4fv("u_View", camera->GetViewMatrix());
+		shader->SetUniformMatrix4fv("u_Projection", camera->GetProjectionMatrix());
+
+		for (auto& mesh : model->GetMeshes())
+		{
+			mesh->Bind();
+			shader->SetUniformMatrix4fv("u_Transform", mesh->GetTransformMatrix());
+
+			glDrawElements(GL_TRIANGLES, mesh->GetIndexCount(), GL_UNSIGNED_INT, nullptr);
+		}
+	}
 
 }
 
--- volkano-gl/OpenGL-Core/src/Renderer.h
+++ volkano-gl/OpenGL-Core/src/Renderer.h
@@ -1,9 +1,29 @@
 #pragma once
+#include "Core/ShaderProgram.h"
+#include "Camera.h"
+#include "Model.h"
 
 namespace glcore {
 
 	class Renderer
 	{
+	public:
+
+		// Remove copy constructor and assignment operator
+		Renderer(const Renderer&) = delete;
+		Renderer& operator=(const Renderer&) = delete;
+
+		static Renderer& Get()
+		{
+			static Renderer instance;
+			return instance;
+		}
+
+		void RenderModel(Model* model, Camera* camera, ShaderProgram* shader);
+
+	private:
+		Renderer();
+
 	};
 
 }
