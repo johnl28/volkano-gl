4e6c1e966f608ac9e81ec68b624e0ca3241f1d62
Johnl <ionutcriste28@gmail.com>
2024-01-16 20:20:36 +0000

    Enable/Disable Skybox

--- volkano-gl/OpenGL-Core/src/Skybox.h
+++ volkano-gl/OpenGL-Core/src/Skybox.h
@@ -11,12 +11,16 @@ namespace glcore {
 	public:
 		Skybox(Model* model, CubeMap* cubemap, ShaderProgram* shader);
 
+		void SetEnabled(bool enabled) { m_Enabled = enabled; }
+		bool IsEnabled() const { return m_Enabled; }
+
 		Model* GetModel() { return m_Model; }
 		CubeMap* GetCubemap() { return m_Cubemap; }
 		ShaderProgram* GetShader() { return m_Shader; }
 
 
 	private:
+		bool m_Enabled = true;
 		Model* m_Model = nullptr;
 		CubeMap* m_Cubemap = nullptr;
 		ShaderProgram* m_Shader = nullptr;
