90a6d77b284846cc3d31cf1d493775658fb8e6dc
Johnl <ionutcriste28@gmail.com>
2024-01-16 20:21:29 +0000

    Particle System Shaders

---
+++ volkano-gl/VolkanoSandbox/assets/shaders/particle_vert.glsl
@@ -0,0 +1,27 @@
+#version 460 core
+
+layout (location = 0) in vec3 aPos;
+layout (location = 1) in vec2 aTextureCoord;
+layout (location = 2) in vec3 aNormals;
+
+out vec2 FragTextureCoord;
+out vec3 FragNormals;
+out vec3 FragPos;
+
+uniform mat4 u_Projection;
+uniform mat4 u_View;
+uniform mat4 u_Transform;
+
+void main()
+{
+	gl_Position = u_Projection * u_View * u_Transform * vec4(aPos, 1.0);
+
+	FragTextureCoord = aTextureCoord;
+
+	FragPos = vec3(u_Transform * vec4(aPos, 1.0));
+
+	// todo: Expensive computation, maybe move it on the CPU side
+	// Book Reference: https://learnopengl.com/Lighting/Basic-Lighting
+	FragNormals = mat3(transpose(inverse(u_Transform))) * aNormals;;
+}
+
