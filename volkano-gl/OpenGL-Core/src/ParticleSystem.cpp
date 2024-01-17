487177031bc6fcf28534d694962709b9b54cb182
Johnl <ionutcriste28@gmail.com>
2024-01-17 18:10:41 +0000

    Particle System refactoring

--- volkano-gl/OpenGL-Core/src/ParticleSystem.cpp
+++ volkano-gl/OpenGL-Core/src/ParticleSystem.cpp
@@ -1,43 +1,26 @@
+
+#include "imgui/imgui.h"
+
+#include "glm/glm.hpp"
 #include "glm/gtc/matrix_transform.hpp"
+#define GLM_ENABLE_EXPERIMENTAL
+#include <glm/gtx/compatibility.hpp>
+
 #include "glad/glad.h"
 
 #include "Log.h"
 #include "ParticleSystem.h"
-
+#include "GlRandom.h"
 
 namespace glcore {
 
-	ParticleSystem::ParticleSystem(Model* model)
+	ParticleSystem::ParticleSystem(Model* model, ShaderProgram* shader)
 	{
 		m_Model = std::unique_ptr<Model>(model);
-		m_Shader = std::make_unique<ShaderProgram>();
-		m_Shader->LoadShaders("assets/shaders/particle_vert.glsl", "assets/shaders/particle_frag.glsl");
+		m_Shader = std::unique_ptr<ShaderProgram>(shader);
+
 
 		m_Particles.resize(1000);
-	}
-
-	void ParticleSystem::OnUpdate(float deltaTime)
-	{
-		for (auto& particle : m_Particles)
-		{
-			if (!particle.IsActive)
-			{
-				continue;
-			}
-			if (particle.LifeRemaining < 0.0f)
-			{
-				particle.IsActive = false;
-				continue;
-			}
-
-			float lifePercentage = 1.0f - particle.LifeRemaining / particle.LifeTime;
-
-			particle.LifeRemaining -= deltaTime;
-			particle.Position += particle.Velocity;
-			particle.Rotation.x += 2.0f;
-			particle.Size = particle.SizeEnd * lifePercentage + 1.0f;
-
-		}
 	}
 
 	void ParticleSystem::Emit(const Particle& newParticle)
@@ -50,10 +33,65 @@ namespace glcore {
 		particle.Position = m_Position;
 		particle.IsActive = true;
 
+		float r = m_Radius / 180.0f;
+
+		particle.Velocity *= glm::vec3(Random::Float(-r, r), 1.0f, Random::Float(-r, r));
+
 		if (--m_ParticleIndex < 0)
 		{
 			m_ParticleIndex = m_Particles.size() - 1;
 		}
+	}
+
+	void ParticleSystem::OnUpdate(float deltaTime)
+	{
+		if (m_Enabled)
+		{
+			Emit(m_ParticleSettings);
+		}
+
+		m_TotalActiveParticles = 0;
+
+		for (auto& particle : m_Particles)
+		{
+			if (!particle.IsActive)
+			{
+				continue;
+			}
+			if (particle.LifeRemaining < 0.0f)
+			{
+				particle.IsActive = false;
+				continue;
+			}
+
+
+			particle.LifeRemaining -= deltaTime;
+			particle.Position += particle.Velocity;
+			particle.Rotation.x += 2.0f * deltaTime;
+
+			++m_TotalActiveParticles;
+		}
+	}
+
+	void ParticleSystem::OnRenderUI()
+	{
+		ImGui::Begin("Volcano Settings");
+
+		ImGui::Checkbox("Enabled", &m_Enabled);
+		ImGui::SliderFloat("Life Time", &m_ParticleSettings.LifeTime, 0.1f, 10.0f);
+		ImGui::SliderFloat("Radius", &m_Radius, 1.0f, 180.0f);
+
+
+		ImGui::SliderFloat("Velocity", &m_ParticleSettings.Velocity.y, 0.1f, 10.0f);
+		ImGui::ColorEdit4("Start Color", &m_ParticleSettings.ColorStart[0]);
+		ImGui::ColorEdit4("End Color", &m_ParticleSettings.ColorEnd[0]);
+
+		ImGui::SliderFloat("Start Size", &m_ParticleSettings.SizeStart, 0.1f, 10.0f);
+		ImGui::SliderFloat("End Size", &m_ParticleSettings.SizeEnd, 0.1f, 10.0f);
+
+		ImGui::Text("Total Particles %d", m_TotalActiveParticles);
+
+		ImGui::End();
 	}
 
 	void ParticleSystem::OnRender(Camera* camera)
@@ -88,11 +126,20 @@ namespace glcore {
 
 			mesh->Bind();
 
+			float lifePercentage = particle.LifeRemaining / particle.LifeTime;
+
+			float scale = glm::lerp(particle.SizeEnd, particle.SizeStart, lifePercentage);
+			auto color = glm::lerp(particle.ColorEnd, particle.ColorStart, lifePercentage);
+
+			//color.a = lifePercentage;
+
+
 			glm::mat4 transformation(1.0f);
 			transformation = glm::translate(transformation, particle.Position);
-			transformation = glm::scale(transformation, glm::vec3(0.01f) * particle.Size);
-
+			transformation = glm::scale(transformation, glm::vec3(0.01f) * scale);
 			transformation = glm::rotate(transformation, particle.Rotation.x, glm::vec3(1.0f, 0, 0));
+
+			m_Shader->SetUniformVec4("u_Color", color);
 			m_Shader->SetUniformMatrix4fv("u_Transform", transformation);
 
 			glDrawElements(GL_TRIANGLES, mesh->GetIndexCount(), GL_UNSIGNED_INT, nullptr);
--- volkano-gl/OpenGL-Core/src/ParticleSystem.h
+++ volkano-gl/OpenGL-Core/src/ParticleSystem.h
@@ -12,13 +12,14 @@ namespace glcore {
 
 	struct Particle
 	{
-		glm::vec3 Position;
-		glm::vec3 Velocity;
+		glm::vec3 Position = glm::vec3(0);
+		glm::vec3 Velocity = glm::vec3(1);
 		glm::vec3 Rotation = glm::vec3(0);
 
-		glm::vec3 Color = glm::vec3(1);
+		glm::vec4 ColorStart = glm::vec4(1);
+		glm::vec4 ColorEnd = glm::vec4(1);
 
-		float Size = 1.0f;
+		float SizeStart = 1.0f;
 		float SizeEnd = 5.0f;
 
 		float LifeRemaining = 0.0f;
@@ -30,20 +31,31 @@ namespace glcore {
 	class ParticleSystem 
 	{
 	public:
-		ParticleSystem(Model* model);
+		ParticleSystem(Model* model, ShaderProgram* shader);
 
+		void OnRenderUI();
 		void OnRender(Camera* camera);
 		void OnUpdate(float deltaTime);
 		void Emit(const Particle& particle);
 
+		void SetParticleSettings(const Particle& particle) { m_ParticleSettings = particle; };
+		const Particle& GetParticleSettings() const { return m_ParticleSettings; }
+
 		void SetPosition(const glm::vec3 position) { m_Position = position; }
 
 	private:
+		Particle m_ParticleSettings;
+
+		
+		bool m_Enabled = true;
 		int m_ParticleIndex = 999;
+		
+		int m_TotalActiveParticles = 0;
+		float m_Radius = 30.0f;
+		glm::vec3 m_Position = glm::vec3(0);
 
 		std::unique_ptr<ShaderProgram> m_Shader = nullptr;
 		std::unique_ptr<Model> m_Model = nullptr;
-		glm::vec3 m_Position = glm::vec3(0);
 		std::vector<Particle> m_Particles;
 	};
 
