7c8de468e5d8f7d7df61f9e17eab5d6fffe79f08
Johnl <ionutcriste28@gmail.com>
2024-01-17 18:09:39 +0000

    Make Random a Singleton

--- volkano-gl/OpenGL-Core/src/GlRandom.h
+++ volkano-gl/OpenGL-Core/src/GlRandom.h
@@ -7,14 +7,37 @@ namespace glcore {
 	class Random
 	{
 	public:
+		const Random& operator=(const Random&) = delete;
+		Random(const Random&) = delete;
+
+		
+		static Random& Get()
+		{
+			static Random instance;
+			return instance;
+		}
+
 		static float Float(float min, float max)
 		{
-			static std::random_device rd;  // Seed with a real random value, if available
-			static std::mt19937 gen(rd()); // Static to maintain state across calls
-			std::uniform_real_distribution<> dis(min, max);
+			return Random::Get().Impl_Float(min, max);
+		}
+
+	private:
+		Random() 
+		{
+			std::random_device randomDevice;
+			gen = std::mt19937(randomDevice());
+		}
+
+		float Impl_Float(float min, float max)
+		{
+
+			std::uniform_real_distribution<float> dis(min, max);
 
 			return dis(gen);
 		}
+
+		std::mt19937 gen;
 	};
 
 }
